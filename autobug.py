ad9a16b03054161c4631b68d96d327c8972c983a
Ahmed Salah Abdalhfaz <mazoka433@gmail.com>
2023-06-05 11:45:20 +0300

    Update autobug.py

--- autobug.py
+++ autobug.py
@@ -1,9 +1,17 @@
 import subprocess
 import time
 
-def run_subfinder(domain):
-    subfinder_command = f"subfinder -d {domain} -all -o subdomains.txt"
+def run_subdomain_scanners(domain):
+    subfinder_command = f"subfinder -d {domain} -o subdomains.txt"
+    findomain_command = f"findomain -t {domain} -u subdomains.txt"
     subprocess.run(subfinder_command, shell=True)
+    subprocess.run(findomain_command, shell=True)
+
+def remove_duplicates():
+    sort_command = "sort subdomains.txt | uniq > subdomains_sorted.txt"
+    mv_command = "mv subdomains_sorted.txt subdomains.txt"
+    subprocess.run(sort_command, shell=True)
+    subprocess.run(mv_command, shell=True)
 
 def run_httprobe():
     httprobe_command = "cat subdomains.txt | httprobe -c 50 -t 3000 > httprobe.txt"
@@ -16,7 +24,8 @@ def run_nuclei():
 if __name__ == '__main__':
     while True:
         domain = input("Enter a domain to scan: ")
-        run_subfinder(domain)
+        run_subdomain_scanners(domain)
+        remove_duplicates()
         run_httprobe()
         run_nuclei()
         time.sleep(3600)  # wait for 1 hour
